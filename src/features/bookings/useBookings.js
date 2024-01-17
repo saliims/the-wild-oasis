a2833c924ce261c4c628a1d10a113a2e27f0e6ed
saliims <saliim.domo@gmail.com>
2024-01-17 19:18:12 +0100

    filter sort but for bookings

---
+++ src/features/bookings/useBookings.js
@@ -0,0 +1,30 @@
+import { useQuery } from "@tanstack/react-query";
+import { getBookings } from "../../services/apiBookings";
+import { useSearchParams } from "react-router-dom";
+
+export function useBookings() {
+  const [searchParams] = useSearchParams();
+
+  //FILTERING
+  const filterValue = searchParams.get("status");
+  const filter =
+    !filterValue || filterValue === "all"
+      ? null
+      : { field: "status", value: filterValue };
+  //{ field: "totalPrice", value: 5000, method: "gte" };
+
+  // SORTING
+  const sortByRaw = searchParams.get("sortBy") || "startDate-desc";
+  const [field, direction] = sortByRaw.split("-");
+  const sortBy = { field, direction };
+
+  const {
+    isLoading,
+    data: bookings,
+    error,
+  } = useQuery({
+    queryKey: ["bookings", filter, sortBy],
+    queryFn: () => getBookings({ filter, sortBy }),
+  });
+  return { isLoading, error, bookings };
+}
